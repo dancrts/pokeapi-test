06c7fb184c4a6019223627a0ee7e61998953225e
dan.crts <oswaldo.daniel@qrsof.com>
2024-11-11 00:17:15 -0600

    Refactored some code, will use it later lmao

--- backend/src/pokemons/pokemons.controller.ts
+++ backend/src/pokemons/pokemons.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response, NextFunction } from "express"
 import * as http from "https"
 import createHttpError from "http-errors";
-import { PokemonResponse } from "./pokemons-response.dto";
+import { PokemonInitialURL, PokemonResponse } from "./pokemons-response.dto";
+
+const sortPokemons = (a: PokemonInitialURL, b: PokemonInitialURL) => { 
+    if (a.name < b.name) return -1; 
+    if(a.name > b.name) return 1; 
+    return 0; 
+}
 
 export const getAllPokemons = (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -10,7 +16,7 @@ export const getAllPokemons = (req: Request, res: Response, next: NextFunction) 
             response.on("data", (chunk) => { data += chunk })
             response.on('end', () => {
                 const fullResponse: PokemonResponse = JSON.parse(data);
-                res.json(fullResponse.results.sort((a, b) => { if (a.name < b.name) return -1; if(a.name > b.name) return 1;  return 0; }));
+                res.json(fullResponse.results.sort(sortPokemons));
             });
         }).on("error", (err) => {
             next(createHttpError(err.message));
